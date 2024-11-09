fd0f49989f86a9570a6e7159692e1a204964046a
Irina-Davidenko <iravi532@gmail.com>
2024-11-09 17:31:14 +0300

    entry point

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import _ from "lodash";
+import getBirthYear from './src/index.js';
 
-console.log("Hello, Hexlet!");
-
-console.log(_.last(["one", "two"]));
+console.log('Сколько вам лет?');
+const age = 18; 
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
